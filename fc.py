411b360b70d2b1b16026fe05ea5bb5e02cd58704
Y. Violet Guo <yueguo598@gmail.com>
2019-11-17 15:30:34 -0500

    FC acc is 56.8 from google colab
    Only trained on subset of 1500 due to RAM limit

--- fc.py
+++ fc.py
@@ -1,95 +1,49 @@
 from sklearn.neural_network import MLPClassifier
 import os
-import nltk
 import numpy as np
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics import confusion_matrix
 import pickle
-from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+
 from warnings import simplefilter
 simplefilter(action='ignore', category=FutureWarning)
 
 seed = 123
 np.random.seed(seed)
+MY_HOME = 'drive/My Drive/'
 
-def tdidf(data):
+def to_tdidf(pickle_path="tdidf_list.pkl"):
+    print("==========> Data Preprocessing")
+    data = pd.read_pickle(os.path.join(MY_HOME, "preprocessed_training_dataframe.pkl"))
+
+    print(data.head())
+
     vectorizer = TfidfVectorizer()
-    vectors = vectorizer.fit_transform(data)
+    vectors = vectorizer.fit_transform(data['X'][0:2000])
     feature_names = vectorizer.get_feature_names()
     dense = vectors.todense()
     denselist = dense.tolist()
     dense_arr = np.array(denselist)
+    fp = os.path.join(MY_HOME, pickle_path)
+    with open(fp, 'wb') as jar:
+        pickle.dump(dense_arr, jar, protocol=pickle.HIGHEST_PROTOCOL)
 
-    return dense_arr
+def fc(denselist, data):
+    clf = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(50), random_state=1)
+    clf.fit(denselist[0:1500], data['label'][0:1500])
 
+    y_pred = clf.predict(X_test)
 
-
-def to_tdidf(pickle_path="tdidf_list_full.pkl"):
-    """
-    process clean text into TF IDF, then dump into a pickle
-    :param pickle_path: pickle file name
-    :return:
-    """
-    print("==========> Data Preprocessing")
-    data = pd.read_pickle("preprocessed_training_dataframe.pkl")
-
-    print(data.head())
-
-
-    X_train, X_test, y_train, y_test = split_tfidf(data['X'][0:500], data['label'][0:500])
-    X_train_tdidf = tdidf(X_train)
-    X_test_tdidf = tdidf(X_test)
-
-
-    del X_train
-    del X_test
-
-    # this poor woman's laptop doesn't have enough ram
-    # keeps killing my process so I now pickle them separately
-
-    with open("tdidf_list_x_train.pkl", 'wb') as jar:
-        pickle.dump(X_train_tdidf, jar, protocol=pickle.HIGHEST_PROTOCOL)
-    with open("tdidf_list_x_test.pkl", 'wb') as jar:
-        pickle.dump(X_test_tdidf, jar, protocol=pickle.HIGHEST_PROTOCOL)
-
-    with open("tdidf_list_y_train.pkl", 'wb') as jar:
-        pickle.dump(y_train, jar, protocol=pickle.HIGHEST_PROTOCOL)
-
-    with open("tdidf_list_y_test.pkl", 'wb') as jar:
-        pickle.dump(y_test, jar, protocol=pickle.HIGHEST_PROTOCOL)
-
-
-def split_tfidf(denselist, label):
-    X = denselist
-    y = label # data['label']# for debugging [0:200]
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-    return X_train, X_test, y_train, y_test
-
-def fc(X_train, X_test, y_train, y_test):
-    clf = MLPClassifier(solver='lbfgs', alpha=2e-3, hidden_layer_sizes=(100), random_state=1)
-    clf.fit(X_train, y_train)
-
-    print(clf.score(X_test, y_test))
-
+    print(classification_report(y_test, y_pred))
 
 if __name__ == '__main__':
-    #to_tdidf()
+    to_tdidf()
+    with open(os.path.join(MY_HOME,"tdidf_list.pkl"), 'rb') as jar:
+        dense_arr = pickle.load(jar)
 
-    with open("tdidf_list_x_train.pkl", 'rb') as jar:
-        X_train = pickle.load(jar)
-
-
-    with open("tdidf_list_x_test.pkl", 'rb') as jar:
-        X_test = pickle.load(jar)
-
-    with open("tdidf_list_y_train.pkl", 'rb') as jar:
-        y_train = pickle.load(jar)
-
-    with open("tdidf_list_y_test.pkl", 'rb') as jar:
-        y_test = pickle.load(jar)
-
-    fc(X_train, X_test, y_train, y_test)
-
-
+    print(dense_arr)
+    data = pd.read_pickle(os.path.join(MY_HOME, "preprocessed_training_dataframe.pkl"))
+    print(data.shape)
+    fc(dense_arr, data)
